e454ba2c41d472218faa96057621146a7e541d9c
zarate10 <zaratelautaro999@gmail.com>
2023-01-29 19:46:36 -0300

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './../assets/css/styles.css'
+
+import ViewGeneradorBoleta from './ViewGeneradorBoleta'
+
+export const Main = () => {
+    return <ViewGeneradorBoleta />
+}
